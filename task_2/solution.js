86fa15855ed9296a5b47564a40283e8fbef13479
Natali Illata <illata@yandex.ru>
2022-04-10 16:59:11 +0500

    task 2.1

--- task_2/solution.js
+++ task_2/solution.js
@@ -7,28 +7,22 @@ function calcShipping(sum, min, shipping) {
 
     // создайте переменную shippingSum
     
-    let shippingSum; 
+    
 
     // если productsSum равно 0,
     // то shippingSum присвоить значение 0
  
-    if (productsSum == 0) {
-        shippingSum = 0;
-    }
-
     // если productsSum Больше или равна freeShippingMinSum,
     // то shippingSum присвоить значение 0
     
-    if (productsSum >= freeShippingMinSum) {
-        shippingSum = 0;
-    }
-
     // если productsSum больше 0 и меньше freeShippingMinSum,
     // то shippingSum присвоить значение shippingPrice
+    let shippingSum; 
     
-    if (productsSum > 0 || productsSum < freeShippingMinSum) {
-        shippingSum = shippingPrice;
-    }
+    if (productsSum == 0 || productsSum >= freeShippingMinSum) {
+        shippingSum = 0
+    } else { shippingSum = shippingPrice;
+    } 
 
     // Конец решения задания №2.1.
 
